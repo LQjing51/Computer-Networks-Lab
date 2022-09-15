bd1be5e3dd2485ce557c4596559144f4349887c8
LQjing51 <2496287199@qq.com>
2022-09-15 18:30:55 +0800

    finish debug

--- lab2/03-socket/test/test.py
+++ lab2/03-socket/test/test.py
@@ -8,29 +8,36 @@ test_dir = dirname(realpath(__file__))
 # http 301
 r = requests.get('http://10.0.0.1/index.html', allow_redirects=False)
 assert(r.status_code == 301 and r.headers['Location'] == 'https://10.0.0.1/index.html')
+print("finish test 1")
 
 # https 200 OK
 r = requests.get('https://10.0.0.1/index.html', verify=False)
 assert(r.status_code == 200 and open(test_dir + '/../index.html', 'rb').read() == r.content)
+print("finish test 2")
 
 # http 200 OK
 r = requests.get('http://10.0.0.1/index.html', verify=False)
 assert(r.status_code == 200 and open(test_dir + '/../index.html', 'rb').read() == r.content)
+print("finish test 3")
 
 # http 404
 r = requests.get('http://10.0.0.1/notfound.html', verify=False)
 assert(r.status_code == 404)
+print("finish test 4")
 
 # file in directory
 r = requests.get('http://10.0.0.1/dir/index.html', verify=False)
 assert(r.status_code == 200 and open(test_dir + '/../index.html', 'rb').read() == r.content)
+print("finish test 5")
 
 # http 206
 headers = { 'Range': 'bytes=100-200' }
 r = requests.get('http://10.0.0.1/index.html', headers=headers, verify=False)
 assert(r.status_code == 206 and open(test_dir + '/../index.html', 'rb').read()[100:201] == r.content)
+print("finish test 6")
 
 # http 206
 headers = { 'Range': 'bytes=100-' }
 r = requests.get('http://10.0.0.1/index.html', headers=headers, verify=False)
 assert(r.status_code == 206 and open(test_dir + '/../index.html', 'rb').read()[100:] == r.content)
+print("finish test 7")
